cd783600022ea560d64c20915bb5606c6746e886
Benjamin Ragheb <ben@benzado.com>
2012-03-19 15:34:54 -0400

    Consolidate SQL statements into hash.

--- lib/mudpie/index.rb
+++ lib/mudpie/index.rb
@@ -4,6 +4,22 @@ class Index
 
   SELECT_FILES_SQL = 'SELECT `id`,`size`,`mtime`,`ymf_len`,`collection_name`,`url`,`date`,`path` FROM `files`'
 
+  SQL = {
+    :select_files => SELECT_FILES_SQL,
+    :select_file_by_path => SELECT_FILES_SQL + ' WHERE `path` = ?',
+    :select_file_by_url => SELECT_FILES_SQL + ' WHERE `url` = ?',
+    :select_files_pages => SELECT_FILES_SQL + " WHERE `ymf_len` > 0 AND `path` NOT LIKE '~_%' ESCAPE '~'",
+    :select_files_posts => SELECT_FILES_SQL + " WHERE `collection_name` = 'posts' ORDER BY `date` DESC",
+    :select_files_md_key_value => SELECT_FILES_SQL + ' JOIN metadata ON metadata.file_id = files.id WHERE `key` = ? AND `value` = ? ORDER BY `date` DESC',
+    :insert_file => 'INSERT INTO `files` (`size`,`mtime`,`ymf_len`,`collection_name`,`url`,`date`,`path`) VALUES (?,?,?,?,?,?,?)',
+    :update_file => 'UPDATE `files` SET `size` = ?,`mtime` = ?,`ymf_len` = ?,`collection_name`=?,`url`=?,`date`=? WHERE `path` = ?',
+    :delete_md => 'DELETE FROM `metadata` WHERE `file_id` = ?',
+    :insert_md => 'INSERT INTO `metadata` (`file_id`,`key`,`index`,`value`) VALUES (?,?,?,?)',
+    :select_md_file_key => 'SELECT `index`,`value` FROM `metadata` WHERE `file_id` = ? AND `key` = ? ORDER BY `index`',
+    :select_md_file => 'SELECT `key`,`index`,`value` FROM `metadata` WHERE `file_id` = ? ORDER BY `key`,`index`',
+    :select_md_values_for_key => 'SELECT `value` FROM metadata WHERE `key` = ? GROUP BY `value`'
+  }
+
   def initialize(site)
     @site = site
     db_path = @site.config['index_path']
@@ -18,13 +34,11 @@ class Index
       end
       @db.execute_batch sql
     end
-    @files_select = @db.prepare(SELECT_FILES_SQL + ' WHERE `path` = ?')
-    @files_insert = @db.prepare('INSERT INTO `files` (`size`,`mtime`,`ymf_len`,`collection_name`,`url`,`date`,`path`) VALUES (?,?,?,?,?,?,?)')
-    @files_update = @db.prepare('UPDATE `files` SET `size` = ?,`mtime` = ?,`ymf_len` = ?,`collection_name`=?,`url`=?,`date`=? WHERE `path` = ?')
-    @md_delete = @db.prepare('DELETE FROM `metadata` WHERE `file_id` = ?')
-    @md_insert = @db.prepare('INSERT INTO `metadata` (`file_id`,`key`,`index`,`value`) VALUES (?,?,?,?)')
-    @md_select_file_key = @db.prepare('SELECT `index`,`value` FROM `metadata` WHERE `file_id` = ? AND `key` = ? ORDER BY `index`')
-    @md_select_file = @db.prepare('SELECT `key`,`index`,`value` FROM `metadata` WHERE `file_id` = ? ORDER BY `key`,`index`')
+    @statements = {}
+  end
+
+  def statement(id)
+    @statements[id] ||= @db.prepare(SQL[id])
   end
 
   def entry_from_row(row, path)
@@ -42,21 +56,21 @@ class Index
 
   def entry_for_path(path)
     entry = nil
-    @files_select.execute!(path) do |row|
+    statement(:select_file_by_path).execute!(path) do |row|
       entry = entry_from_row row, path
     end
     entry
   end
 
   def save_entry(entry)
-    if entry.id.nil? then @files_insert else @files_update end.execute!(
+    statement(if entry.id.nil? then :insert_file else :update_file end).execute!(
       entry.size, entry.mtime, entry.ymf_len, entry.collection_name,
       entry.url, entry.date, entry.path
     )
     if entry.id.nil? then
       entry.id = @db.last_insert_row_id
     else
-      @md_delete.execute!(entry.id)
+      statement(:delete_md).execute!(entry.id)
     end
   end
 
@@ -64,16 +78,16 @@ class Index
     data.each_pair do |key,value|
       if value.is_a? Array then
         value.each_index do |i|
-          @md_insert.execute!(file_id, key, i, value[i])
+          statement(:insert_md).execute!(file_id, key, i, value[i])
         end
       elsif value.is_a? Hash then
         puts "WARNING: YAML key `#{key}`: hashes not searchable."
       elsif value.is_a? TrueClass then
-        @md_insert.execute!(file_id, key, nil, 1)
+        statement(:insert_md).execute!(file_id, key, nil, 1)
       elsif value.is_a? FalseClass then
-        @md_insert.execute!(file_id, key, nil, 0)
+        statement(:insert_md).execute!(file_id, key, nil, 0)
       else
-        @md_insert.execute!(file_id, key, nil, value)
+        statement(:insert_md).execute!(file_id, key, nil, value)
       end
     end
   end
@@ -109,8 +123,7 @@ class Index
 
   def page_for_url(url)
     page = nil
-    stmt = @db.prepare(SELECT_FILES_SQL + ' WHERE `url` = ?')
-    stmt.execute!(url) do |row|
+    statement(:select_file_by_url).execute!(url) do |row|
       entry = entry_from_row row, row[7]
       page = Page.new(@site, entry)
     end
@@ -120,7 +133,7 @@ class Index
   # unused
   def ymf_data_for_file_id(id)
     ymf_data = {}
-    @md_select_file.execute!(file_id) do |row|
+    statement(:select_md_file).execute!(file_id) do |row|
       key = row[0]
       index = row[1]
       value = row[2]
@@ -138,7 +151,7 @@ class Index
   # unused
   def ymf_value_for_file_id_and_key(id, key)
     values = []
-    @md_select_file_key.execute!(file_id, key) do |row|
+    statement(:select_md_file_key).execute!(file_id, key) do |row|
       index = row[0]
       value = row[1]
       return value if index.nil?
@@ -152,7 +165,7 @@ class Index
   end
 
   def each(stmt = nil)
-    (stmt || @db.prepare(SELECT_FILES_SQL)).execute! do |row|
+    (stmt || statement(:select_files)).execute! do |row|
       entry = entry_from_row row, row[7]
       yield Page.new(@site, entry)
     end
@@ -165,19 +178,15 @@ class Index
   end
 
   def all_pages
-    where = " WHERE `ymf_len` > 0 AND `path` NOT LIKE '~_%' ESCAPE '~'"
-    all @db.prepare(SELECT_FILES_SQL + where)
+    all statement(:select_files_pages)
   end
 
   def all_posts
-    where = " WHERE `collection_name` = 'posts' ORDER BY `date` DESC"
-    all @db.prepare(SELECT_FILES_SQL + where)
+    all statement(:select_files_posts)
   end
 
   def all_for_key_and_value(key, value)
-    join_sql = ' JOIN metadata ON metadata.file_id = files.id'
-    where_sql = ' WHERE `key` = ? AND `value` = ? ORDER BY `date` DESC'
-    stmt = @db.prepare(SELECT_FILES_SQL + join_sql + where_sql)
+    stmt = statement(:select_files_md_key_value)
     stmt.bind_param 1, key
     stmt.bind_param 2, value
     all stmt
@@ -185,8 +194,7 @@ class Index
 
   def all_known_values_for_key(key)
     values = []
-    query = 'SELECT `value` FROM metadata WHERE `key` = ? GROUP BY `value`'
-    @db.execute(query, key) do |row|
+    statement(:select_md_values_for_key).execute!(key) do |row|
       values << row[0]
     end
     values
